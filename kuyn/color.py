91214b941180279c3d6519a6dea70b750f0a43d3
vemboy <vem@papazian.info>
2019-08-20 15:46:45 +0300

    Update color.py
    Error checking

--- kuyn/color.py
+++ kuyn/color.py
@@ -1,4 +1,22 @@
 import math
+import sys
+
+def error_checking(Color):
+    
+    if(Color.r > 255):
+        raise Exception("Red cannot be higher than 255")
+         
+    elif(Color.g > 255):
+        raise Exception("Green cannot be higher than 255")
+         
+    elif(Color.b > 255):
+        raise Exception("Blue cannot be higher than 255")
+         
+    elif(Color.a > 1.0):
+        raise Exception("Alpha cannot be higher than 1.0")
+    else:
+        return(Color.r, Color.g, Color.b, Color.a)
+        
 
 class Color:
     def __init__(self, r: int, g: int, b: int, a: float = 1.0) -> None:
@@ -11,13 +29,19 @@ class Color:
     def magnitude(self):
         return math.sqrt((self.r - 0)^2 + (self.g - 0)^2 + (self.b - 0)^2)
 
+
+        
+
     def __add__(self, other):
-        """ comment """
+        
         if isinstance(other, int):
-            return Color(r=self.r + other, g=self.g + other, b=self.b + other, a=self.a + other)
+            
+            error_checking(Color(r=self.r + other, g=self.g + other, b=self.b + other, a=self.a + other))
+                
 
         elif isinstance(other, Color):
-            return Color(r=self.r + other.r, g=self.g + other.g, b=self.b + other.b, a=self.a + other.a)
+            
+            error_checking(Color(r=self.r + other.r, g=self.g + other.g, b=self.b + other.b, a=self.a + other.a))
 
         else:
             raise Exception("Added type is not supported.")
@@ -25,10 +49,12 @@ class Color:
     def __sub__(self, other):
         
         if isinstance(other, int):
-            return Color(r=self.r - other, g=self.g - other, b=self.b - other, a=self.a - other)
+            
+            error_checking(Color(r=self.r - other, g=self.g - other, b=self.b - other, a=self.a - other))
 
         elif isinstance(other, Color):
-            return Color(r=self.r - other.r, g=self.g - other.g, b=self.b - other.b, a=self.a - other.a)
+            
+            error_checking(Color(r=self.r - other.r, g=self.g - other.g, b=self.b - other.b, a=self.a - other.a))
 
         else:
             raise Exception("Subtracted type is not supported.")
@@ -42,4 +68,5 @@ class Color:
 
     def __mul__(self, scalar: float):
     
-        return Color(r=self.a * scalar, g=self.r * scalar, b=self.g * scalar, a=self.b * scalar)
+        error_checking(Color(r=self.a * scalar, g=self.r * scalar, b=self.g * scalar, a=self.b * scalar))
+
